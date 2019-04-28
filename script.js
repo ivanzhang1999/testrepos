55095d884dccc9ec9ee0bffb7db3784c3d92c22d
ivanzhang1999 <50060416+ivanzhang1999@users.noreply.github.com>
2019-04-27 21:33:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changePara()
+{
+    document.getElementById("heading").innerHTML =
+    "Hat is matte black";
+    document.getElementById("para").innerHTML =
+    "Got the boots that's black to match";
+    document.getElementById("image").src =
+    "https://media3.s-nbcnews.com/j/newscms/2019_15/2815291/190409-lil-nas-x-mn-1310_011f346519297b6bb6e95c5a5856293a.fit-760w.jpg"
+}
